ce7456e3cf005852a54d0038dcc9bda33952b5bd
Gaurav Setia <gaurav.singh.setia@gmail.com>
2019-06-23 22:44:22 -0400

    navbar 2

--- src/index.js
+++ src/index.js
@@ -19,15 +19,42 @@ class App extends React.Component {
   render(){
     return(
       <div>
+      
+      <body className="w3-black">
+            <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat"/>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 
-        <title> Gaurav </title>
+      <nav className="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center w3-black navBar">
         
-        {this.state.shouldRender ? <Tester/> : null}
-        
-        
-        
-        
-        </div>
+
+        <a href="#home" className="w3-bar-item w3-button w3-padding-large w3-black w3-hover-grey navBarButtons">
+          <i className="fa fa-home w3-xxlarge"></i>
+          <p>HOME</p>
+        </a>
+        <a href="#applications" className="w3-bar-item w3-button w3-padding-large w3-black w3-hover-grey navBarButtons">
+          <i className="fa fa-star w3-xxlarge"></i>
+          <p>APPS</p>
+        </a>
+        <a href="#about" className="w3-bar-item w3-button w3-padding-large w3-black w3-hover-grey navBarButtons">
+          <i className="fa fa-user w3-xxlarge"></i>
+          <p>ABOUT</p>
+        </a>
+        <a href="#contact" className="w3-bar-item w3-button w3-padding-large w3-black w3-hover-grey navBarButtons">
+          <i className="fa fa-envelope w3-xxlarge"></i>
+          <p>CONTACT</p>
+        </a>
+
+      </nav>
+      </body>
+            
+              
+      {this.state.shouldRender ? <Tester/> : null}
+              
+              
+              
+              
+              </div>
       );
   }
 }
